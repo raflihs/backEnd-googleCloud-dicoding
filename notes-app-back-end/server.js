19bb3a12d74e1d680b569f36fa366c12bf4bed31
raflihs <60882820+raflihs@users.noreply.github.com>
2023-03-16 08:31:04 +0700

    Update port Server.js
    perubahan port server.js

--- notes-app-back-end/server.js
+++ notes-app-back-end/server.js
@@ -3,7 +3,7 @@ const routes = require("./routes.js");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3001,
+    port: 5000,
     host: "localhost",
     routes: {
       cors: {
